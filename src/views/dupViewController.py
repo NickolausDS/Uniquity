5479e50c99fe36b87455342e3c19b08b6272780f
Nickolaus Saint <nick@incendiarysoftware.com>
2014-04-11 17:18:54 -0700

    Added some empty methods, for basic design work

---
+++ src/views/dupViewController.py
@@ -0,0 +1,23 @@
+
+class DuplicateFileViewController(object):
+	
+	def __init__(self):
+		pass
+		
+	#Change to a different view
+	def changeView(self, newView):
+		pass
+		
+	#Output operations
+	#The model has data to output to the screen	
+	
+	
+	def updateDups(self, files):
+		pass
+		
+	#Input operations
+	#The user clicked on some part of the view, and we require data from the view
+	def getSelected(self, files):
+		pass
+		
+	
