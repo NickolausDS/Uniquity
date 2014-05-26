60d3edb8ac34c7d1000147e56c1f69e6af515f79
Nickolaus Saint <nick@incendiarysoftware.com>
2014-05-26 13:35:12 -0700

    Simplified the startup script

--- src/Uniquity.py
+++ src/Uniquity.py
@@ -1,19 +1,3 @@
-import sys
-import os
-import wx
-
-
-if getattr(sys, 'frozen', False):
-    # we are running in a |PyInstaller| bundle
-    BASEPATH = os.path.dirname(sys.executable)
-else:
-    # we are running in a normal Python environment
-    BASEPATH = os.path.dirname(__file__)
-
-#Add the path
-sys.path.insert(0, os.path.abspath(BASEPATH))
-
-#With path setup, we can finally import what we need from the project
 from views import controller
 
 #Initialize the main Uniquity GUI
